28654239eea05e85539c9e258c45397752c19187
daon1213 <diaeiyo1213@naver.com>
2022-09-02 11:02:33 +0900

    🔧- Navigation 화면으로 넘겨주는 역할 및 Worker 함수 init

---
+++ app/src/main/java/com/daon/dive/presentation/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.daon.dive.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import androidx.work.*
+import com.daon.dive.R
+import com.daon.dive.databinding.ActivityMainBinding
+import com.daon.dive.work.TrackingCheckWorker
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        initView()
+        initWorker()
+    }
+
+    private fun initView() {
+        val navigationController =
+            (supportFragmentManager.findFragmentById(R.id.mainNavigationHostContainer) as NavHostFragment).navController
+        binding.toolbar.setupWithNavController(navigationController)
+    }
+
+    private fun initWorker() {
+        val workerStartTime = Calendar.getInstance()
+        workerStartTime.set(Calendar.HOUR_OF_DAY, 16)
+        val initialDelay = workerStartTime.timeInMillis - System.currentTimeMillis()
+        val dailyTrackingCheckRequest =
+            PeriodicWorkRequestBuilder<TrackingCheckWorker>(1, TimeUnit.DAYS)
+                .setInitialDelay(initialDelay, TimeUnit.MILLISECONDS)
+                .setBackoffCriteria(
+                    BackoffPolicy.LINEAR,
+                    PeriodicWorkRequest.MIN_BACKOFF_MILLIS,
+                    TimeUnit.MILLISECONDS
+                )
+                .build()
+
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "DailyTrackingCheck",
+                ExistingPeriodicWorkPolicy.KEEP,
+                dailyTrackingCheckRequest
+            )
+    }
+}
