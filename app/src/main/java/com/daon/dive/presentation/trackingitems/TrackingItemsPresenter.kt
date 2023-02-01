a6e288df3e2a56ddff9c345c464658fab99d0bb8
daon1213 <diaeiyo1213@naver.com>
2023-02-01 16:08:45 +0900

    🔧 Lifecycle 제거 및 널 자유화

--- app/src/main/java/com/daon/dive/presentation/trackingitems/TrackingItemsPresenter.kt
+++ app/src/main/java/com/daon/dive/presentation/trackingitems/TrackingItemsPresenter.kt
@@ -1,22 +1,22 @@
 package com.daon.dive.presentation.trackingitems
 
-import androidx.lifecycle.LifecycleCoroutineScope
 import com.daon.dive.data.entity.TrackingInformation
 import com.daon.dive.data.entity.TrackingItem
 import com.daon.dive.data.repository.TrackingItemRepository
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class TrackingItemsPresenter(
-    private val view: TrackingItemsContract.View,
+    private val view: TrackingItemsContract.View?,
     private val trackingItemRepository: TrackingItemRepository
 ) : TrackingItemsContract.Presenter {
 
     override var trackingItemInformation: List<Pair<TrackingItem, TrackingInformation>> = emptyList()
 
-    override val scope: LifecycleCoroutineScope = MainScope() as LifecycleCoroutineScope
+    override val scope: CoroutineScope = MainScope()
 
     init {
         trackingItemRepository
@@ -37,21 +37,21 @@ class TrackingItemsPresenter(
 
     private fun fetchTrackingInformation(forceFetch: Boolean = false) = scope.launch {
         try {
-            view.showLoadingIndicator()
+            view?.showLoadingIndicator()
 
             if (trackingItemInformation.isEmpty() || forceFetch) {
                 trackingItemInformation = trackingItemRepository.getTrackingItemInformation()
             }
 
             if (trackingItemInformation.isEmpty()) {
-                view.showNoDataDescription()
+                view?.showNoDataDescription()
             } else {
-                view.showTrackingItemInformation(trackingItemInformation)
+                view?.showTrackingItemInformation(trackingItemInformation)
             }
         } catch (exception: Exception) {
             exception.printStackTrace()
         } finally {
-            view.hideLoadingIndicator()
+            view?.hideLoadingIndicator()
         }
     }
 }
